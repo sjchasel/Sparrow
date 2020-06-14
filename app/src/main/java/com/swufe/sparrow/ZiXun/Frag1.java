44c892027fed0d59aa5d8cf8698b9969be4655bb
sjchasel <1296841437@qq.com>
2020-06-14 22:56:09 +0800

    frag1可以每日刷新了！嘎嘎嘎嘎嘎嘎嘎

--- app/src/main/java/com/swufe/sparrow/ZiXun/Frag1.java
+++ app/src/main/java/com/swufe/sparrow/ZiXun/Frag1.java
@@ -6,8 +6,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +18,8 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.swufe.sparrow.R;
 
 import org.jsoup.Jsoup;
@@ -38,12 +38,15 @@ import java.util.Map;
 public class Frag1 extends ListFragment implements AdapterView.OnItemClickListener {
 
     private ListView listView;
-    Handler handler;
+    private DownloadTask mTask;
+    String todayStr;
     private SimpleAdapter listItemAdapter;
-    private ArrayList<HashMap<String, Object>> listItems;
+    //private ArrayList<HashMap<String, Object>> listItems;
     private int flag;
     private ProgressDialog progressDialog;
     String TAG = "Frag1";
+    SharedPreferences sharedPreferences;
+    List<HashMap<String, String>> testList;
 
     public Frag1() {
 
@@ -55,55 +58,41 @@ public class Frag1 extends ListFragment implements AdapterView.OnItemClickListen
         initListView();//数据初始化
         this.setListAdapter(listItemAdapter);//将list的适配器设置为listItemAdapter
         listView.setOnItemClickListener(this);//为list设置点击事件
-        //保存数据的sp
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("myTest", Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();//要获得一个edit对象才可以改变sharedPreferences
-        editor.commit();
-        //gengxinpinlv(sharedPreferences);
-
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                if (msg.what == 3) {
-
-                    List<HashMap<String, String>> tList = (List<HashMap<String, String>>) msg.obj;
+        //tList = new ArrayList<HashMap<String, String>>();
 //                    ListAdapter adapter = new ArrayAdapter<String>(TestListActivity.this, android.R.layout.simple_list_item_1, tList);
 //                    setListAdapter(adapter);
 //                    Log.i("handler", "reset list...");
-                    SimpleAdapter adapter = new SimpleAdapter(getActivity(), tList, // listItems数据源
-                            R.layout.item1, // ListItem的XML布局实现
-                            new String[]{"Title", "url"},
-                            new int[]{R.id.title, R.id.url});
-                    setListAdapter(adapter);
-                    Log.i("handler", "reset list...");
+//        listItemAdapter = new SimpleAdapter(getActivity(), testList, // listItems数据源
+//                R.layout.item1, // ListItem的XML布局实现
+//                new String[]{"Title", "url"},
+//                new int[]{R.id.title, R.id.url});
+//        listView.setAdapter(listItemAdapter);
 
-                }
-                super.handleMessage(msg);
-            }
-        };
+
+        //存数据的sp（list要转成json才能存）
+        sharedPreferences = getActivity().getSharedPreferences("myTest", Activity.MODE_PRIVATE);
+
 
         return view;
     }
 
     private void initListView() {
-        listItems = new ArrayList<HashMap<String, Object>>();
-
-        HashMap<String, Object> map = new HashMap<String, Object>();
+        //listItems = new ArrayList<HashMap<String, Object>>();
+        testList = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> map = new HashMap<String, String>();
         map.put("Title", "wait..."); // 标题文字
-        listItems.add(map);
+        testList.add(map);
 
         // 生成适配器的Item和动态数组对应的元素
-        listItemAdapter = new SimpleAdapter(getActivity(), listItems, // listItems数据源
+        listItemAdapter = new SimpleAdapter(getActivity(), testList, // listItems数据源
                 R.layout.item1,
                 new String[]{"Title", "url"},
                 new int[]{R.id.title, R.id.url}
         );
+        listView.setAdapter(listItemAdapter);
     }
 
-    public List<Map<String, Object>> getData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        return list;
-    }
+
 
 
     @Override
@@ -112,7 +101,49 @@ public class Frag1 extends ListFragment implements AdapterView.OnItemClickListen
     }
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        new DownloadTask().execute();
+
+        sharedPreferences = getActivity().getSharedPreferences("myTest", Activity.MODE_PRIVATE);
+        String updateDate = sharedPreferences.getString("update_date","");
+        Log.i(TAG, "onActivityCreated: 上次存储的时间是"+updateDate);
+        //获取当前系统时间
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        todayStr = sdf.format(today);
+        Log.i(TAG, "onActivityCreated: 当前时间是" + todayStr);
+        //判断时间
+        if(!todayStr.equals(updateDate)){
+
+            Log.i(TAG, "onActivityCreated: 需要更新");
+            //开启异步处理
+            mTask = new DownloadTask();
+            mTask.execute();
+            //this.setListAdapter(listItemAdapter);
+            SharedPreferences sp = getActivity().getSharedPreferences("myTest", Activity.MODE_PRIVATE);
+            String listJson = sp.getString("KEY_USER_DATA","");
+            Gson gson = new Gson();
+            testList = gson.fromJson(listJson, new TypeToken<List<HashMap>>(){
+            }.getType());
+
+
+        }else{
+            SharedPreferences sp = getActivity().getSharedPreferences("myTest", Activity.MODE_PRIVATE);
+            String listJson = sp.getString("KEY_USER_DATA","");
+            Log.i(TAG, "onActivityCreated: listJson"+listJson);
+            if(!listJson.equals("")){
+                Gson gson = new Gson();
+                testList = gson.fromJson(listJson, new TypeToken<List<HashMap>>(){
+                }.getType());
+                Log.i(TAG, "onActivityCreated:testList "+testList);
+                listItemAdapter = new SimpleAdapter(getActivity(), testList, // listItems数据源
+                        R.layout.item1,
+                        new String[]{"Title", "url"},
+                        new int[]{R.id.title, R.id.url}
+                );
+                setListAdapter(listItemAdapter);
+
+            }
+            Log.i(TAG, "onCreate: 不需要更新");
+        }
 
 
     }
@@ -122,9 +153,7 @@ public class Frag1 extends ListFragment implements AdapterView.OnItemClickListen
         try {
             //从ListView中获取选中数据
             HashMap<String, String> map = (HashMap<String, String>) getListView().getItemAtPosition(position);
-            String titleStr = map.get("Title");
             String u = map.get("url");
-
             //打开新的页面
             Uri uri = Uri.parse(u);
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
@@ -134,55 +163,37 @@ public class Frag1 extends ListFragment implements AdapterView.OnItemClickListen
         }
     }
 
-//    public void gengxinpinlv(SharedPreferences sharedPreferences){
-//        String updateDate = sharedPreferences.getString("update_date","");
-//
-//        //获取当前系统时间
-//        Date today = Calendar.getInstance().getTime();
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        final String todayStr = sdf.format(today);
-//
-//        Log.i(TAG, "onCreate: sp updateDate=" + updateDate);
-//        Log.i(TAG, "onCreate: todayStr=" + todayStr);
-//
-////判断时间
-//        if(!todayStr.equals(updateDate)){
-//            Log.i(TAG, "onCreate: 需要更新");
-//            //开启子线程
-//            Thread t = new Thread();
-//            t.start();
-//        }else{
-//            Log.i(TAG, "onCreate: 不需要更新");
-//        }
-//    }
 
-    class DownloadTask extends AsyncTask<Integer, Integer, Void> {
 
+    class DownloadTask extends AsyncTask<Integer, Integer, List<HashMap<String, String>>> {
         @Override
-        protected Void doInBackground(Integer... integers) {
-            Log.i("thread", "run... ");
-            List<HashMap<String, String>> testList = new ArrayList<HashMap<String, String>>();
+        protected List<HashMap<String, String>> doInBackground(Integer...integers) {
+            Log.i(TAG, "doInBackground: 异步处理");
+            List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
             try {
-                //Thread.sleep(1000);
+                Document doc = Jsoup.connect("https://tophub.today/n/Y3QeLGAd7k").get();
+                Elements als = doc.getElementsByClass("al");
+                for (int i = 0; i <= 19; i++) {
+                    //获取链接
+                    String content = als.get(i).getElementsByTag("a").attr("href");
+                    Log.i("Frag1", "run:链接是 " + content);
+                    //新建hashmap
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    map.put("url", content);//把链接放入
+                    //获取标题
+                    String spanStr = als.get(i).text();
+                    Log.i("title", spanStr);
+                    map.put("Title", spanStr);//放入标题
 
-                        Document doc = Jsoup.connect("https://tophub.today/n/Y3QeLGAd7k").get();
-                        Elements als = doc.getElementsByClass("al");
-                        for (int i = 0; i <= 19; i++) {
-                            String content = als.get(i).getElementsByTag("a").attr("href");
-                            Log.i("Frag2", "run: " + content);
-                            HashMap<String, String> map = new HashMap<String, String>();
-                            map.put("url", content);
-                            //testList.add(map);
-                            String spanStr = als.get(i).text();
-                            Log.i("title", spanStr);
-                            map.put("Title", spanStr);
-                            testList.add(map);
-                            //putHashMapData(getActivity(), "data", map);
-                        }
+                    Log.i(TAG, "doInBackground: 此时map的内容是"+map);
+                    //map存入list
+                    list.add(map);
+
+                    //把testList转成json存入；把当前时间存入。
 
 
 
-
+                }
             } catch (MalformedURLException e) {
                 Log.e("www", e.toString());
                 e.printStackTrace();
@@ -190,15 +201,14 @@ public class Frag1 extends ListFragment implements AdapterView.OnItemClickListen
                 Log.e("www", e.toString());
                 e.printStackTrace();
             }
-//            catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-            Message msg = handler.obtainMessage(3);
-            msg.obj = testList;
-            handler.sendMessage(msg);
-            Log.i("thread", "sendMessage.....");
-
-            return null;
+            Gson user_gson = new Gson();
+            String user_jsonStr = user_gson.toJson(list);
+            SharedPreferences.Editor editor = sharedPreferences.edit();//要获得一个edit对象才可以改变sharedPreferences
+            editor.putString("KEY_USER_DATA",user_jsonStr);
+            editor.putString("update_date",todayStr);
+            Log.i(TAG, "doInBackground: 已存入当前时间"+todayStr);
+            editor.apply();
+            return list;
         }
 
         @Override
@@ -209,18 +219,32 @@ public class Frag1 extends ListFragment implements AdapterView.OnItemClickListen
 
         @Override
         protected void onProgressUpdate(Integer... values) {
-            //progressDialog.setMessage("当前下载进度：" + values[0] + "%");
             Toast.makeText(getActivity(), "当前下载进度：" + values[0] + "%", Toast.LENGTH_SHORT).show();
         }
 
 
-        protected void onPostExecute(Boolean result) {
-            //progressDialog.dismiss();
-            if (result) {
-                Toast.makeText(getActivity(), "更新成功", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getActivity(), "更新失败", Toast.LENGTH_SHORT).show();
+        protected void onPostExecute(List<HashMap<String, String>> result) {
+            if(result==null||result.size()==0){
+                Log.i(TAG, "onPostExecute: hashmap为空");
+            }else {
+                if (testList == null){
+                    testList = new ArrayList<>();
+                }
+                for (HashMap<String, String> li : result) {
+                    testList.add(li);
+                    //将下载的数据添加到list容器后，刷新每个Item的内容
+                    listItemAdapter.notifyDataSetChanged();
+                    Log.i(TAG, "onPostExecute: 已通知adapter去刷新");
+                    listItemAdapter = new SimpleAdapter(getActivity(), testList, // listItems数据源
+                            R.layout.item1,
+                            new String[]{"Title", "url"},
+                            new int[]{R.id.title, R.id.url}
+                    );
+                    setListAdapter(listItemAdapter);
+                }
+                super.onPostExecute(result);
             }
+
         }
     }
 }
